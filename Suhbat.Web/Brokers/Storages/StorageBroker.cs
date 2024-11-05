97fc229e48ef415291bd36626dd0be040cf224cc
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-11-05 15:22:02 +0500

    DATA: Models & Migrate Users and Messages

---
+++ Suhbat.Web/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,43 @@
+﻿//--------------------------------------------------
+// Copyright (c) Coalition Of Good-Hearted Engineers
+// Free To Use To Find Comfort And Peace
+//--------------------------------------------------
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Suhbat.Web.Models.Foundations.Messages;
+
+namespace Suhbat.Web.Brokers.Storages
+{
+    public partial class StorageBroker:DbContext
+    {
+        private readonly IConfiguration configuration;
+
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            Database.Migrate();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = 
+                this.configuration.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Message>()
+                .HasOne(m => m.Sender)
+                .WithMany()
+                .HasForeignKey(m => m.SenderId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Message>()
+                .HasOne(m => m.Receiver)
+                .WithMany()
+                .HasForeignKey(m => m.ReceiverId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
